3775e4e26004827341caa0e217b1e312dccb0790
梅世祺 <shiqimei@kugou.net>
2023-01-29 16:09:31 +0800

    chore: init project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('#app') as HTMLCanvasElement
+const ctx = canvas.getContext('2d')!
+
+ctx.fillStyle = '#fffefc'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
